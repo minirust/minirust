c501b9b5abbe4c86ad3d50ac9bb6903530d8865c
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-28 13:30:30 +0100

    new ci

--- ci.sh
+++ ci.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -ex
 
-# Fixed `minirust-tooling` commit, we need to bump this occasionally.
-TOOLING_COMMIT="338caeb"
+# Fixed specr-transpile version
+VERSION="0.1.4"
 
-# where to check out the tooling
-TOOLING_DIR="$HOME/minirust-tooling"
+cargo install "specr-transpile@${VERSION}"
+specr-transpile specr.toml
 
-git clone "https://github.com/memoryleak47/minirust-tooling" "$TOOLING_DIR"
-ln -s "$PWD" "$TOOLING_DIR"/minirust
-
-cd "$TOOLING_DIR"
-git checkout "$TOOLING_COMMIT"
-
-# transpile, and build the transpiled result
-(cd specr-transpile; cargo run)
-(cd gen-minirust; RUSTFLAGS="-D warnings" cargo build)
+cd gen-minirust
+RUSTFLAGS="-D warnings" cargo build
