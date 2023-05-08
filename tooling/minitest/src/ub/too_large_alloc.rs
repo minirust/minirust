31933bf43c5cfe9a27eb3bed2e376fe21f5be12e
Yannik Wyss <yawyss@student.ethz.ch>
2023-05-08 10:20:05 +0200

    fix test too_large_alloc, local wrong type.

--- tooling/minitest/src/ub/too_large_alloc.rs
+++ tooling/minitest/src/ub/too_large_alloc.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 #[test]
 fn too_large_alloc() {
-    let locals = vec![<usize>::get_ptype()];
+    let locals = vec![<*const usize>::get_ptype()];
     let b = block!(
         storage_live(0),
         allocate(const_int::<usize>(usize::MAX / 2 + 1), const_int::<usize>(1), local(0), 1),
