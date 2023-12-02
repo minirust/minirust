ca0ea41c8bbbf92e330bef79d7787e95f713beb2
Timon Meyer <timeyer@ethz.ch>
2023-12-02 16:00:48 +0100

    add discriminant tests

---
+++ tooling/minitest/src/tests/enum_discriminant.rs
@@ -0,0 +1,98 @@
+use crate::*;
+
+#[test]
+fn discriminant_get_and_set_work() {
+    // single-variant enum without data and the tag 4 for the only variant
+    let enum_ty = enum_ty(
+        &[enum_variant(tuple_ty(&[], size(1), align(1)), &[(size(0), 4)])],
+        Discriminator::Branch { offset: size(0), fallback: GcCow::new(Discriminator::Invalid), children: [(4, Discriminator::Known(0.into()))].into_iter().collect() },
+        1,
+        size(1),
+        align(1)
+    );
+    let locals = [enum_ty];
+
+    // check that discriminant matches whats written, go to unreachable if not
+    let blocks = [
+        block!(
+            storage_live(0),
+            set_discriminant(local(0), 0),
+            if_(eq(get_discriminant(local(0)), const_int(0u8)), 1, 2)
+        ),
+        block!(exit()),
+        block!(unreachable()),
+    ];
+    let function = function(Ret::No, 0, &locals, &blocks);
+    let program = program(&[function]);
+    assert_stop(program);
+}
+
+
+#[test]
+fn discriminant_setting_right_value() {
+    // multi-variant enum without data and the tags 4 and 2.
+    let enum_ty = enum_ty(
+        &[
+            enum_variant(tuple_ty(&[], size(1), align(1)), &[(size(0), 4)]),
+            enum_variant(tuple_ty(&[], size(1), align(1)), &[(size(0), 2)]),
+        ],
+        Discriminator::Branch { offset: size(0), fallback: GcCow::new(Discriminator::Invalid), children: [(4, Discriminator::Known(0.into()))].into_iter().collect() },
+        1,
+        size(1),
+        align(1)
+    );
+    let locals = [union_ty(&[(size(0), enum_ty), (size(0), int_ty(Signedness::Unsigned, size(1)))], size(1), align(1))];
+
+    // check that discriminant matches whats written, go to unreachable if not
+    let blocks = [
+        block!(
+            storage_live(0),
+            set_discriminant(field(local(0), 0), 0),
+            if_(eq(load(field(local(0), 1)), const_int(4u8)), 1, 3)
+        ),
+        block!(
+            set_discriminant(field(local(0), 0), 1),
+            if_(eq(load(field(local(0), 1)), const_int(2u8)), 2, 3)
+        ),
+        block!(exit()),
+        block!(unreachable()),
+    ];
+    let function = function(Ret::No, 0, &locals, &blocks);
+    let program = program(&[function]);
+    assert_stop(program);
+}
+
+#[test]
+/// Tests the integrity of the enum data after set_discriminant.
+fn discriminant_leaves_data_alone() {
+    let u8_t = int_ty(Signedness::Unsigned, size(1));
+    let u16_t = int_ty(Signedness::Unsigned, size(2));
+
+    // single-variant enum with layout <u8 data, u8 tag, u16 data> and tag 1
+    let enum_ty = enum_ty(
+        &[enum_variant(tuple_ty(&[(size(0), u8_t), (size(2), u16_t)], size(4), align(2)), &[(size(1), 1)])],
+        Discriminator::Branch { offset: size(1), fallback: GcCow::new(Discriminator::Invalid), children: [].into_iter().collect() },
+        1, size(4), align(2)
+    );
+    // the only local is a union of the enum and all its field seperately
+    let locals = [union_ty(&[(size(0), enum_ty), (size(0), u8_t), (size(1), u8_t), (size(2), u16_t)], size(4), align(2))];
+
+    let blocks = [
+        block!(
+            // setup enum
+            storage_live(0),
+            assign(field(downcast(field(local(0), 0), 0), 0), const_int(12u8)),
+            assign(field(downcast(field(local(0), 0), 0), 1), const_int(9834u16)),
+            set_discriminant(field(local(0), 0), 0),
+            // now let the checks begin
+            if_(eq(load(field(local(0), 1)), const_int(12u8)), 1, 4)
+        ),
+        block!(if_(eq(load(field(local(0), 2)), const_int(1u8)), 2, 4)),
+        block!(if_(eq(load(field(local(0), 3)), const_int(9834u16)), 3, 4)),
+        block!(exit()),
+        block!(unreachable())
+    ];
+    let function = function(Ret::No, 0, &locals, &blocks);
+    let program = program(&[function]);
+    assert_stop(program);
+}
