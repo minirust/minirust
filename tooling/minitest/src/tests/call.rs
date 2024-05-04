920fd98a914054805ced1414855db405e3516134
Ralf Jung <post@ralfj.de>
2024-05-04 15:15:07 +0200

    add test for return type mismatch
    Fixes https://github.com/minirust/minirust/issues/116

--- tooling/minitest/src/tests/call.rs
+++ tooling/minitest/src/tests/call.rs
@@ -111,3 +111,28 @@ fn call_ret_abi() {
     dump_program(p);
     assert_ub(p, "call ABI violation: return types are not compatible");
 }
+
+#[test]
+fn ret_mismatch() {
+    // function that returns a u64
+    let other_f = {
+        let locals = [<u64>::get_type()];
+        let b0 = block!(assign(local(0), const_int::<u64>(0)), return_());
+
+        function(Ret::Yes, 0, &locals, &[b0])
+    };
+
+    let locals = [<u8>::get_type()];
+
+    let b0 = block!(
+        storage_live(0),
+        // call to the function with a u8 place to put the value into.
+        call(1, &[], local(0), Some(1))
+    );
+    let b1 = block!(exit());
+
+    let f = function(Ret::No, 0, &locals, &[b0, b1]);
+    let p = program(&[f, other_f]);
+    dump_program(p);
+    assert_ub(p, "call ABI violation: return types are not compatible");
+}
