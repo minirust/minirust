dd9838b2479b748084c77c9c8c5093d3ba8b69c1
Ralf Jung <post@ralfj.de>
2024-05-14 13:49:34 +0200

    add tuple comparison test
    fixes #157

--- tooling/minimize/tests/pass/tuples.rs
+++ tooling/minimize/tests/pass/tuples.rs
@@ -4,6 +4,10 @@ use intrinsics::*;
 fn main() {
     let x = 30;
     print(foo(x-12).1);
+
+    // Also test tuple comparison.
+    assert!((1, 0) != (0, 1));
+    assert!((1, 0) == (1, 0));
 }
 
 fn foo(x: i32) -> (i32, i32) {
