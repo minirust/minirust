21b4d372a9b10962a086cf28a2a4c643e6851c66
sun-jacobi <sun1011jacobi@gmail.com>
2024-07-23 14:18:59 +0200

    add tests/ub/tree_borrows/mutable_ref_conflicting_write_read.rs

--- tooling/minimize/tests/ub/tree_borrows/mutable_ref_conflicting_read_write.rs
+++ tooling/minimize/tests/ub/tree_borrows/mutable_ref_conflicting_read_write.rs
@@ -3,7 +3,7 @@ fn main() {
     let x = &mut 31; // (x, Reserved)   
     let xraw = x as *mut i32; // (x, Reserved) 
     let y = unsafe { &mut *xraw }; // (x, Reserved) -> (y, Reserved)
-    *y = 42; // (x, Reserved) -> (y, Active)
-    assert!(unsafe { *xraw } == 42); // (x, Reserved) -> (y, Frozen)
+    *y = 42; // (x, Active) -> (y, Active)
+    assert!(unsafe { *xraw } == 42); // (x, Active) -> (y, Frozen)
     *y = 31; // UB! Child Write to Frozen 
 }
---
+++ tooling/minimize/tests/ub/tree_borrows/mutable_ref_conflicting_write_write.rs
@@ -0,0 +1,14 @@
+//@ compile-flags: --minimize-tree-borrows
+fn main() {
+    let x = &mut 31; // (x, Reserved)   
+    let xraw = x as *mut i32; // (x, Reserved) 
+    let y = unsafe { &mut *xraw }; // (x, Reserved) -> (y, Reserved)
+    *y = 42; // (x, Active) -> (y, Active)
+
+    unsafe { 
+        *xraw = 57; // (x, Active) -> (y, Disabled)
+        assert!(*xraw == 57); // (x, Active) -> (y, Disabled)
+    } 
+
+    *y = 31; // UB! Child Write to Disabled 
+}
