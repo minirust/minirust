ac29eefb952b38e20621ae3509e2fa71b6ae1575
sun-jacobi <sun1011jacobi@gmail.com>
2024-08-09 17:55:58 +0200

    add non_accessed_foreign_write_frozen

--- tooling/minimize/tests/pass/tree_borrows/non_accessed_protector.rs
+++ tooling/minimize/tests/pass/tree_borrows/non_accessed_protector.rs
@@ -2,15 +2,34 @@
 
 // Check that non-accessed protected nodes do not cause UB on foreign writes.
 
-fn foo(x: *mut u8, _y: &mut u8) {
-    // (x, P[[R, R]]) -> (y, P[[R, R]])
-    unsafe { *x.add(1) = 57; } // Okay to Foreign Write to y
-}
-
-fn main() {
+fn non_accessed_foreign_write_reserved() {
+    fn foo(x: *mut u8, _y: &mut u8) {
+        // (x, P[[R, R]]) -> (y, P[[R, R]])
+        unsafe { *x.add(1) = 57; } // Okay to Foreign Write to y
+    }
+    
     let mut data = [42u8, 57]; 
     let x = &mut data[0] as *mut u8; // (x, [R, R]) 
     let y = unsafe { &mut *x }; // (x, [R, R]) -> (y, [R, R])
     
     foo(x, y);
+}
+
+fn non_accessed_foreign_write_frozen() {
+    fn foo(x: *mut u8, _y: &u8) {
+        // (x, P[[R, R]]) -> (y, P[[F, F]])
+        unsafe { *x.add(1) = 57; } // Okay to Foreign Write to y
+    }
+    
+    let mut data = [42u8, 57]; 
+    let x = &mut data[0] as *mut u8; // (x, [R, R]) 
+    let y = unsafe { &*x }; // (x, [R, R]) -> (y, [F, F])
+
+    foo(x, y);
+}
+
+
+fn main() {
+    non_accessed_foreign_write_reserved();
+    non_accessed_foreign_write_frozen();
 }
