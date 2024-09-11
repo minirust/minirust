0f2dd14cc27a5dd06dde0a8f60d60536acb8a5c8
Ralf Jung <post@ralfj.de>
2024-09-11 16:21:28 +0200

    make ptr_add_overflow test more tricky

--- tooling/minimize/tests/ub/ptr_add_overflow.rs
+++ tooling/minimize/tests/ub/ptr_add_overflow.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let data = [1u16, 2, 3, 4, 5];
-    let first = &data[0] as *const u16;
-    unsafe {
-        let _ptr = first.add(2).add(-1i32 as usize);
-    }
+    let x = &[0i32; 2];
+    let x = std::ptr::from_ref(&x[0]).wrapping_add(1);
+    // Will be equal to -4isize when multiplied be the size (4) -- and that step does not itself overflow.
+    let offset = !0usize >> 2;
+    // However, the usize-to-isize cast is lossy and hence this should be UB.
+    // Or put differently, -4isize as usize is out-of-bounds.
+    unsafe { x.add(offset).read() };
 }
