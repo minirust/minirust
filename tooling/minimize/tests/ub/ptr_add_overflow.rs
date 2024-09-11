b967a966d313d1f8fae651bddbc3cc4c326e2e5c
Ralf Jung <post@ralfj.de>
2024-09-11 17:11:41 +0200

    make the test more clear

--- tooling/minimize/tests/ub/ptr_add_overflow.rs
+++ tooling/minimize/tests/ub/ptr_add_overflow.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let x = &[0i32; 2];
+    let x = &[0u8; 2];
     let x = std::ptr::from_ref(&x[0]).wrapping_add(1);
-    // Will be equal to -4isize when multiplied be the size (4) -- and that step does not itself overflow.
-    let offset = !0usize >> 2;
-    // However, the usize-to-isize cast is lossy and hence this should be UB.
-    // Or put differently, -4isize as usize is out-of-bounds.
-    unsafe { x.add(offset).read() };
+    // If the `!0` is interpreted as `isize`, it is just `-1` and hence harmless.
+    // However, this is unsigned arithmetic, so really this is `usize::MAX` and hence UB.
+    unsafe { x.add(!0).read() };
 }
