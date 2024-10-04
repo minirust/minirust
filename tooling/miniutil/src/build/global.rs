653566b2b861ff0a9e33af80da1377642248a0b7
Cyrill Bucher <indeximal@gmail.com>
2024-10-04 22:58:57 +0200

    Make miniutil compile again. Some test fail with FIXME.

--- tooling/miniutil/src/build/global.rs
+++ tooling/miniutil/src/build/global.rs
@@ -2,8 +2,12 @@ use crate::build::*;
 
 impl ProgramBuilder {
     pub fn declare_global_zero_initialized<T: TypeConv>(&mut self) -> PlaceExpr {
-        let bytes = List::from_elem(Some(0), T::get_size().expect_sized("T is `Sized`").bytes());
-        let global = Global { bytes, relocations: list!(), align: <T>::get_align() };
+        let bytes = List::from_elem(Some(0), T::get_layout().expect_size("T is `Sized`").bytes());
+        let global = Global {
+            bytes,
+            relocations: list!(),
+            align: <T>::get_layout().expect_align("T is `Sized`"),
+        };
         let name = GlobalName(Name::from_internal(self.next_global));
         self.next_global += 1;
         self.globals.try_insert(name, global).unwrap();
@@ -13,15 +17,19 @@ impl ProgramBuilder {
 
 /// Global Int initialized to zero.
 pub fn global_int<T: TypeConv>() -> Global {
-    let bytes = List::from_elem(Some(0), T::get_size().expect_sized("T is `Sized`").bytes());
+    let bytes = List::from_elem(Some(0), T::get_layout().expect_size("T is `Sized`").bytes());
 
-    Global { bytes, relocations: list!(), align: T::get_align() }
+    Global { bytes, relocations: list!(), align: T::get_layout().expect_align("T is `Sized`") }
 }
 
 /// Global pointer
 pub fn global_ptr<T: TypeConv + ?Sized>() -> Global {
     let bytes =
-        List::from_elem(Some(0), <*const T>::get_size().expect_sized("*T is `Sized`").bytes());
+        List::from_elem(Some(0), <*const T>::get_layout().expect_size("*T is `Sized`").bytes());
 
-    Global { bytes, relocations: list!(), align: <*const T>::get_align() }
+    Global {
+        bytes,
+        relocations: list!(),
+        align: <*const T>::get_layout().expect_align("T is `Sized`"),
+    }
 }
