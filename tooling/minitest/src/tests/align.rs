64504dcfc81f451295596d6ace5f623d64789dd5
Cyrill Bucher <indeximal@gmail.com>
2024-10-28 20:07:17 +0100

    fix error messages in tests and semantic change

--- tooling/minitest/src/tests/align.rs
+++ tooling/minitest/src/tests/align.rs
@@ -136,7 +136,7 @@ fn deref_misaligned_ref() {
     );
     let f = function(Ret::No, 0, &locals, &[b0, b1]);
     let p = program(&[f]);
-    assert_ub::<BasicMem>(p, "transmuted value is not valid at new type");
+    assert_ub::<BasicMem>(p, "Value::Ptr: unaligned safe pointer");
 }
 
 #[test]
