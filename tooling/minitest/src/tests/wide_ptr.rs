e3f2772e7aaf18aa24f236b2cb50fb3c78b262ab
Ralf Jung <post@ralfj.de>
2025-01-06 10:29:02 +0100

    tweak comment

--- tooling/minitest/src/tests/wide_ptr.rs
+++ tooling/minitest/src/tests/wide_ptr.rs
@@ -15,7 +15,8 @@ fn ub_wide_thin_abi_incompatible() {
         let mut f = p.declare_function();
         let x = f.declare_local::<u32>();
         f.storage_live(x);
-        // UB: &u32 and &[u32] are different size and thus certainly not abi compatible.
+        // UB: `*const u32` and `*const [u32]` are different size and thus certainly
+        // not ABI compatible.
         f.call_ignoreret(fn_ptr(foo), &[by_value(addr_of(x, <*const u32>::get_type()))]);
         f.exit();
         p.finish_function(f)
