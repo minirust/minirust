ad2696db9d23a78b1e76ca563ccb4dd2c0653e9c
Max Smith <149544637+max-t-smith@users.noreply.github.com>
2025-11-13 16:17:23 +0100

    Update tooling/minimize/tests/ui.rs
    Co-authored-by: Ralf Jung <post@ralfj.de>

--- tooling/minimize/tests/ui.rs
+++ tooling/minimize/tests/ui.rs
@@ -36,9 +36,8 @@ fn cfg(path: &str, mode: Mode) -> Config {
     // Change cargo command from `build` to `check`.
     dependency_program.args[0] = "check".into();
 
-    // Get the minimize binary to point at as the rustc
+    // Use minimize itself as RUSTC, which will set the right flags.
     let minimize_exe = PathBuf::from(env!("CARGO_BIN_EXE_minimize"));
-
     dependency_program.envs.push(("RUSTC".into(), Some(minimize_exe.into())));
     dependency_program.envs.push(("MINIMIZE_BE_RUSTC".into(), Some("deps".into())));
     dependency_program.envs.push(("MINIMIZE_BUILD_SYSROOT".into(), Some("off".into())));
